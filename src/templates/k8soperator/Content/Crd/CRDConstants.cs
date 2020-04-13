3dd131c1ae2824139138b885e4480afc524e2a62
Xavier Granollers <xavier.granollers@gmail.com>
2020-04-13 17:12:00 +0200

    Issue #9 Group name is not populated from template params to constants

--- src/templates/k8soperator/Content/Crd/CRDConstants.cs
+++ src/templates/k8soperator/Content/Crd/CRDConstants.cs
@@ -2,7 +2,7 @@
 {
     internal class CRDConstants
     {
-        public const string Group = "xabaril.io";
+        public const string Group = "{{crdlowergroupname}}";
         public const string Version = "v1";
         public const string Plural = "{{crdlowername}}s";
     }
