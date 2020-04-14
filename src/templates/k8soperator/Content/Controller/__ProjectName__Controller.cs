2435c16cfd188d5458cb20972d383ccc445aca37
Xavier Granollers <xavier.granollers@gmail.com>
2020-04-14 09:32:57 +0200

    Issue 14 : Create a sample to show a real k8s operator
    Added links to the project Healthkchecks as suggested.

--- src/templates/k8soperator/Content/Controller/__ProjectName__Controller.cs
+++ src/templates/k8soperator/Content/Controller/__ProjectName__Controller.cs
@@ -4,6 +4,9 @@ using __ProjectName__.Diagnostics;
 using System;
 using System.Threading.Tasks;
 
+// Check this example to get a better understanding
+// https://github.com/Xabaril/AspNetCore.Diagnostics.HealthChecks/
+// https://github.com/Xabaril/AspNetCore.Diagnostics.HealthChecks/tree/master/src/HealthChecks.UI.K8s.Operator/Controller
 namespace __ProjectName__.Controller
 {
     public class __ProjectName__Controller
@@ -24,6 +27,7 @@ namespace __ProjectName__.Controller
         {
             _diagnostics.ControllerDo();
 
+            //add your code here
             return new ValueTask();
         }
 
@@ -31,6 +35,7 @@ namespace __ProjectName__.Controller
         {
             _diagnostics.ControllerUnDo();
 
+            // delete/dispose needed stuff
             return new ValueTask();
         }
     }
